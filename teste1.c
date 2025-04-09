c626209eeccf332ec0ce2cae6e5576b73c0586d0
DanPrOgRaMaCaO <dan75yz7@gmail.com>
2025-04-09 21:31:40 +0000

    em andamento

---
+++ teste1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+/*
+soma (+)
+subtracao (-)
+sultiplicacao (*)
+divisão (/)
+*/
+
+int numero1, numero2;
+int soma, subtracao, multiplicacao, divisao;
+
+// operação soma
+soma = numero1 + numero2;
+
+// operação subtracao
+subtracao = numero1 - numero2;
+
+// operação multiplicacao
+multiplicacao = numero1 * numero2;
+
+// operação divisao
+divisao = numero1 / numero1;
+
+printf("entre com o numero 1:\n");
+scanf("%d", &numero1);
+printf("entre com o numero 2:\n");
+scanf("%d", &numero2);
+
+// operação soma
+soma = numero1 + numero2;
+
+// operação subtracao
+subtracao = numero1 - numero2;
+
+// operação multiplicacao
+multiplicacao = numero1 * numero2;
+
+// operação divisao
+divisao = numero1 / numero2;
+
+printf("A soma é:%d\n", soma);
+printf("A subtração é:%d\n", subtracao);
+printf("A multiplicação é:%d\n", multiplicacao);
+printf("A divisão é:%d\n", divisao);
+return 0;
+}
---
+++ teste2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    Atribuição Simples (=)
+    Atribuição com Soma (+=)
+    Atribuição com Subtração (-=)
+    Atribuição com Multiplicação (*=)
+    Atribuição com Divisão (/=)
+    */
+
+    int numero1, numero2, resultado;
+
+    resultado = 10;
+    printf("Resultado: %d\n", resultado);
+
+    //resultado = resultado + 20
+    resultado += 20;
+    printf("Resultado: %d\n", resultado);
+    
+    //resultado = resultado - numero1
+    resultado -= numero1;
+
+    resultado *= 5; 
+
+}
